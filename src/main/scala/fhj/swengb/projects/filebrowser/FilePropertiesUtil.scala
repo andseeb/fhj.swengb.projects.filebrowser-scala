12c02ecee431a2fe90a2c4348e5addc4fe513f49
Folk Daniel <daniel.folk@edu.fh-joanneum.at>
2016-01-15 14:05:24 +0100

    MD Files

--- src/main/scala/fhj/swengb/projects/filebrowser/FilePropertiesUtil.scala
+++ src/main/scala/fhj/swengb/projects/filebrowser/FilePropertiesUtil.scala
@@ -9,7 +9,8 @@ object FilePropertiesUtil {
       file.getName.endsWith(".cfg") | file.getName.endsWith(".config") |
       file.getName.endsWith(".scala") | file.getName.endsWith(".java") |
       file.getName.endsWith(".html") | file.getName.endsWith(".xhtml") |
-      file.getName.endsWith(".xml") | file.getName.endsWith(".json") | file.getName.endsWith(".csv")
+      file.getName.endsWith(".xml") | file.getName.endsWith(".json") |
+      file.getName.endsWith(".csv") | file.getName.endsWith(".md")
   }
   // TODO: create more file definitions
 }
